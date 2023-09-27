ce5af8baa287557ffac176f6df6c93d9db4f0cab
User <User@DESKTOP-CG4RU7C>
2023-09-27 16:56:38 +0900

    xml 추가 패키지 추가

---
+++ Helpu/src/main/java/test1/tests1.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class tests1 {
+
+	private int num;
+	private String title;
+	private String writer;
+	private String filename;
+	private String content;
+	private String b_date;
+	
+}
