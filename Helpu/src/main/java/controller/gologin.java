64dbd4dc938d6e433473aec94ba67eac1af599be
User <User@DESKTOP-CG4RU7C>
2023-10-06 17:37:50 +0900

    페이지 이동, 로그인 깡통틀, db연결,AAA폴더 삭제,페이지 이동 파일명 변경

---
+++ Helpu/src/main/java/controller/gologin.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class gologin implements Command{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		return "WEB-INF/login.jsp";
+	}
+
+}
