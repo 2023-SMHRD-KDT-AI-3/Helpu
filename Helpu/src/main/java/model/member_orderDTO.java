c4a411e6b275108f9daa46ebf7ac92c3372a0621
smhrd <smhrd@DESKTOP-1JO17I6>
2023-10-17 17:15:36 +0900

    구매내역 데이터 추가작업중

---
+++ Helpu/src/main/java/model/member_orderDTO.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class member_orderDTO {
+
+	private int pro_code;
+	private String pro_name;
+	private int pro_price;
+	private String order_at;
+	private int od_cnt;
+	
+	
+}
